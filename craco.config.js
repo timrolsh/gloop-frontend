417b97a6fc74e1df61c9483d981f8efd9df1ba36
Tim Rolsh <tim.rolsh@gmail.com>
2025-06-08 19:58:51 -0400

    Add polyfill for events module to fix cloudflare failed build

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,21 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   webpack: {
+    configure: (webpackConfig, {env, paths}) => {
+      // Add the fallback for the 'e
+      // vents' module
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback, // Spread existing fallbacks
+        events: require.resolve("events/")
+      };
+
+      return webpackConfig;
+    },
+
     alias: {
-      '~': path.resolve(__dirname, 'src'),
-      '@': path.resolve(__dirname, 'src')
+      "~": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src")
     }
   }
-}
+};
