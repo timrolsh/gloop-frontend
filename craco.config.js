836e582ac959ecac7e0c3a648fb737296fa06080
Tim Rolsh <tim.rolsh@gmail.com>
2025-06-08 21:22:02 -0400

    Revert "Add build to gitignore, update packages to remove build warnings"
    This reverts commit 0f3fb8b7c1b20cde925584b1244ceb4e1987f7e8.

--- craco.config.js
+++ craco.config.js
@@ -1,31 +1,10 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig, {env, paths}) => {
-      // Add the fallback for the 'e
-      // vents' module
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback, // Spread existing fallbacks
-        events: require.resolve("events/"),
-        "crypto": require.resolve("crypto-browserify"),
-        "buffer": require.resolve("buffer/"),
-        "stream": require.resolve("stream-browserify"),
-        "http": require.resolve("stream-http"),
-        "httpsa": require.resolve("https-browserify"),
-        // "os": require.resolve("os-browserify/browser"),
-        "url": require.resolve("url"),
-        "zlib": require.resolve("zlib-browserify"),
-        "assert": require.resolve("assert/"),
-        "path": require.resolve("path-browserify")
-      };
-
-      return webpackConfig;
-    },
-
     alias: {
-      "~": path.resolve(__dirname, "src"),
-      "@": path.resolve(__dirname, "src")
+      '~': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src')
     }
   }
-};
+}
