fe0e462cb5843d9fd833f3745d38e7c0f55f2f49
Tim Rolsh <tim.rolsh@gmail.com>
2025-08-14 01:59:47 -0400

    Remove leaderboard page from being locked

--- src/consts/routes.js
+++ src/consts/routes.js
@@ -2,7 +2,7 @@ export const routes = {
   Home: {name: "Home", path: "/", locked: false, authenticated: false},
   Dashboard: {name: "Dashboard", path: "/dashboard", locked: false, authenticated: false},
   Gmi: {name: "GMI", path: "/gmi", locked: false, authenticated: false},
-  Leaderboard: {name: "Leaderboard", path: "/leaderboard", locked: true, authenticated: false},
+  Leaderboard: {name: "Leaderboard", path: "/leaderboard", locked: false, authenticated: false},
   Loop: {name: "Loop", path: "/loop", locked: false, authenticated: false},
   Liquidate: {name: "Liquidate", path: "/liquidate", locked: false, authenticated: false},
   Borrow: {name: "Deposit/Borrow", path: "/borrow", locked: false, authenticated: false},
