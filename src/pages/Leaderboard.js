f45be88c3ad4345d7cb36db58bef199715132ee8
Tim Rolsh <tim.rolsh@gmail.com>
2025-10-19 23:10:11 -0500

    Add no lock staking boost 10% to leaderboard page description

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -28,7 +28,7 @@ export default function Leaderboard() {
                 <div className="color-white font-16 my-2">
                   Earn points for participating in Gloop's growing ecosystem by lending USDC and
                   borrowing USDC. Current Rates and Boosts: Lending USDC: 2 points per dollar per
-                  day, Borrowing USDC: 1 point per dollar per day. Staking Boosts: 14 days - 25%, 28
+                  day, Borrowing USDC: 1 point per dollar per day. Staking Boosts: 0 days - 10%, 14 days - 25%, 28
                   days - 50%, 56 days - 100%.
                 </div>
               </div>
